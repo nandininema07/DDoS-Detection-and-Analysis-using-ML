8a3e47189856d76dea81bc8bdd00ac714dd3726b
nandininema07 <nandini.nema027@gmail.com>
2025-04-10 03:11:51 +0530

    database

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,10 +5,12 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     phone = db.Column(db.String(15))
-    password = db.Column(db.String(100))
+    password = db.Column(db.String(100), nullable=False)
+    api_key = db.Column(db.String(120), unique=True, nullable=False) 
+
 
 class Log(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,6 +19,7 @@ class Log(db.Model):
     status = db.Column(db.String(20))  # Safe, Flagged, DDoS
     confidence = db.Column(db.Float)
     details = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class Setting(db.Model):
     id = db.Column(db.Integer, primary_key=True)
